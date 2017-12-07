9b87b627c723b092be714af847009e8bb4c62f5c
ruskeezy <christian.ax.miller@gmail.com>
2017-12-07 14:08:03 -0800

    wrapped questions in functions

--- app.js
+++ app.js
@@ -1,95 +1,111 @@
 'use strict';
 
-var twin = prompt('Does Bessie have a twin?');
-console.log('Twin Guess:', twin);
+function hasTwin() {
+  var twin = prompt('Does Bessie have a twin?');
+  console.log('Twin Guess:', twin);
 
-if(twin.toUpperCase() === 'YES' || twin.toLowerCase() === 'y') {
-  alert('Yes! She has a twin sister!');
-} else if(twin.toUpperCase() === 'NO' || twin.toLowerCase() === 'n') {
-  alert('Wrong! She has a twin sister.');
-} else {
-  alert('Sorry, I didn\'t recognize your answer.')
-}
-
-var drive = prompt('Does Bessie drive a Range rover?');
-console.log('Car guess:', drive);
-
-if(drive.toUpperCase() === 'NO' || drive.toLowerCase() === 'n') {
-  alert('Correct, she drives a KIA Soul.');
-} else if(drive.toUpperCase() === 'YES' || drive.toLowerCase() === 'y') {
-  alert('Wrong! That car is way too expensive!');
-} else {
-  alert('Sorry, I didn\'t recognize your answer.')
-}
-
-var travel = prompt('Has Bessie ever traveled outside of the country?');
-console.log('Travel guess:', travel);
-
-if(travel.toUpperCase() === 'YES' || travel.toLowerCase() === 'y') {
-  alert('Correct! She has been to the Philippines.');
-} else if(travel.toUpperCase() === 'NO' || travel.toLowerCase() === 'n') {
-  alert('WRONG! She\'s been to The Phillpines and Canada.');
-} else {
-  alert('Sorry, I didn\'t recognize your answer.')
-}
-
-var born = prompt('Was Bessie born in Seattle?');
-console.log('Born guess:', born)
-
-if(born.toUpperCase() === 'YES' || born.toLowerCase() === 'y') {
-  alert('Yes! She was born at the UWMC.');
-} else if(born.toUpperCase() === 'NO' || born.toLowerCase() === 'n') {
-  alert('You guessed wrong, she was born at UWMC.');
-} else {
-  alert('Sorry, I didn\'t recognize your answer.')
-}
-
-var dog = prompt('Is Bessie\'s favorite dog the Belgian Malinois?');
-console.log('Dog guess:', dog);
-
-if(drive.toUpperCase() === 'NO' || drive.toLowerCase() === 'n') {
-  alert('Correct, her favorite dog is the Welsh Pembroke Corgi.');
-} else if(drive.toUpperCase() === 'YES' || drive.toLowerCase() === 'y') {
-  alert('No way! She LOVES corgis.');
-} else {
-  alert('Sorry, I didn\'t recognize your answer.')
-}
-
-var year;
-
-for (var i = 0; i < 4; i++) {
-  year = parseInt(prompt('What year did Bessie get her drivers license?'));
-  console.log('Year guessed:', year);
-
-  if (year === 2006) {
-    alert('Correct!');
-    break;
-  } else if (year < 2006) {
-    alert('Sorry, she got it after that year.');
-  } else if (year > 2006) {
-    alert('Sorry, she got it before that year.');
-  } else if (isNaN(year) || year === null) {
-    alert('That isn\'t a year.');
+  if(twin.toUpperCase() === 'YES' || twin.toLowerCase() === 'y') {
+    alert('Yes! She has a twin sister!');
+  } else if(twin.toUpperCase() === 'NO' || twin.toLowerCase() === 'n') {
+    alert('Wrong! She has a twin sister.');
+  } else {
+    alert('Sorry, I didn\'t recognize your answer.')
   }
 }
 
+function rangeRover() {
+  var drive = prompt('Does Bessie drive a Range rover?');
+  console.log('Car guess:', drive);
 
-var guesses = 6;
-var food = ['pizza', 'mac and cheese', 'sphaghetti', 'ramen', 'sushi', 'burgers']
+  if(drive.toUpperCase() === 'NO' || drive.toLowerCase() === 'n') {
+    alert('Correct, she drives a KIA Soul.');
+  } else if(drive.toUpperCase() === 'YES' || drive.toLowerCase() === 'y') {
+    alert('Wrong! That car is way too expensive!');
+  } else {
+    alert('Sorry, I didn\'t recognize your answer.')
+  }
+}
 
-while (guesses > 0) {
-  var answer = prompt('What is one of my favorite foods?');
-  guesses--;
+function hasTravelled() {
+  var travel = prompt('Has Bessie ever traveled outside of the country?');
+  console.log('Travel guess:', travel);
 
-  for (var i = 0; i < food.length; i++) {
-    if (answer === food[i]) {
+  if(travel.toUpperCase() === 'YES' || travel.toLowerCase() === 'y') {
+    alert('Correct! She has been to the Philippines.');
+  } else if(travel.toUpperCase() === 'NO' || travel.toLowerCase() === 'n') {
+    alert('WRONG! She\'s been to The Phillpines and Canada.');
+  } else {
+    alert('Sorry, I didn\'t recognize your answer.')
+  }
+}
+
+function birthPlace() {
+  var born = prompt('Was Bessie born in Seattle?');
+  console.log('Born guess:', born)
+
+  if(born.toUpperCase() === 'YES' || born.toLowerCase() === 'y') {
+    alert('Yes! She was born at the UWMC.');
+  } else if(born.toUpperCase() === 'NO' || born.toLowerCase() === 'n') {
+    alert('You guessed wrong, she was born at UWMC.');
+  } else {
+    alert('Sorry, I didn\'t recognize your answer.')
+  }
+}
+
+function favoriteDog() {
+  var dog = prompt('Is Bessie\'s favorite dog the Belgian Malinois?');
+  console.log('Dog guess:', dog);
+
+  if(drive.toUpperCase() === 'NO' || drive.toLowerCase() === 'n') {
+    alert('Correct, her favorite dog is the Welsh Pembroke Corgi.');
+  } else if(drive.toUpperCase() === 'YES' || drive.toLowerCase() === 'y') {
+    alert('No way! She LOVES corgis.');
+  } else {
+    alert('Sorry, I didn\'t recognize your answer.')
+  }
+}
+
+function license() {
+  var year;
+
+  for (var i = 0; i < 4; i++) {
+    year = parseInt(prompt('What year did Bessie get her drivers license?'));
+    console.log('Year guessed:', year);
+
+    if (year === 2006) {
       alert('Correct!');
-      guesses = -1;
       break;
+    } else if (year < 2006) {
+      alert('Sorry, she got it after that year.');
+    } else if (year > 2006) {
+      alert('Sorry, she got it before that year.');
+    } else if (isNaN(year) || year === null) {
+      alert('That isn\'t a year.');
     }
   }
+}
 
-  if (guesses > 0) {
-    alert('Oops! Try again.');
+function favoriteFoods() {
+  var guesses = 6;
+  var food = ['pizza', 'mac and cheese', 'sphaghetti', 'ramen', 'sushi', 'burgers']
+
+  while (guesses > 0) {
+    var answer = prompt('What is one of my favorite foods?');
+    guesses--;
+
+    for (var i = 0; i < food.length; i++) {
+      if (answer === food[i]) {
+        alert('Correct!');
+        guesses = -1;
+        break;
+      }
+    }
+    if (guesses > 0) {
+      alert('Oops! Try again.');
+    }
   }
 }
+
+
+hasTwin();
+rangeRover();
